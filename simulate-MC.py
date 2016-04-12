d05520760e14958f089cf526c551a209a2bbffca
huayijing <huayi@nzrs.net.nz>
2016-04-13 11:19:24 +1200

    Create simulate-MC.py

---
+++ simulate-MC.py
@@ -0,0 +1,19 @@
+import numpy as np
+import quantecon as qe
+
+def mc_sample_path(P, init=0, sample_size=1000):
+    # === make sure P is a NumPy array === #
+    P = np.asarray(P)
+    # === allocate memory === #
+    X = np.empty(sample_size, dtype=int)
+    X[0] = init
+    # === convert each row of P into a distribution === #
+    # In particular, P_dist[i] = the distribution corresponding to P[i,:]
+    n = len(P)
+    P_dist = [qe.DiscreteRV(P[i,:]) for i in range(n)]
+
+    # === generate the sample path === #
+    for t in range(sample_size - 1):
+        X[t+1] = P_dist[X[t]].draw()
+
+    return X
